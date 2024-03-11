30532f11ac6d8ce810fb7471333c4770ee9c944a
ALi <Sayavdera@protonmail.com>
2024-03-11 20:17:46 +0500

    adding Noxam Config COmmand Initilization and adding NoxamAgressiveOptimize.hxx Headers File

--- Config/NoxamCommand.cxx
+++ Config/NoxamCommand.cxx
@@ -1,3 +1,66 @@
+#include <iostream>
+#include <string>
 
+#include <windows.h>
+#include <stdio.h>
 
 #include "../Headers/Config/NoxamCommand.hxx"
+#include "../Headers/Crypto/NoxamStringOprations.hxx"
+#include "../Headers/Crypto/NoxamUrlEncoding.hxx"
+
+
+using namespace std;
+
+/*
+|===========================================================
+|    NoxamCreateCommand()
+|===========================================================
+|
+|
+|===========================================================
+*/
+char* NoxamCreateCommand(int NxmCommandID, int NxmCommandType, char *NxmData, int NxmDataLength)
+{
+
+}
+
+/*
+|===========================================================
+|    NoxamParseCommand()
+|===========================================================
+|
+|
+|===========================================================
+*/
+int NoxamParseCommand(char* NxmCommand, char** NxmCommandData, int* NxmCommandType)
+{
+
+}
+
+/*
+|===========================================================
+|    NoxamFreeCommand()
+|===========================================================
+|
+|
+|===========================================================
+*/
+void NoxamFreeCommand(char *NxmData)
+{
+
+}
+
+/*
+|===========================================================
+|    NoxamFreeParseCommandResult()
+|===========================================================
+|
+|
+|===========================================================
+*/
+void NoxamFreeParseCommandResult(char *NxmData)
+{
+
+}
+
+
--- Headers/Config/NoxamCommand.hxx
+++ Headers/Config/NoxamCommand.hxx
@@ -1,4 +1,32 @@
 #ifndef NOXAM_NOXAMCOMMAND_HXX
 #define NOXAM_NOXAMCOMMAND_HXX
 
+
+#define NOXAM_COMMAND_JOIN 0
+#define NOXAM_COMMAND_POLL 1
+#define NOXAM_COMMAND_CHECKIFSITEISUP 2
+#define NOXAM_COMMAND_SHUTDOWN 3
+#define NOXAM_COMMAND_RESTART 4
+#define NOXAM_COMMAND_MESSAGEBOX 5
+#define NOXAM_COMMAND_KEYLOG 6
+#define NOXAM_COMMAND_REMOTEPROCESS 7
+#define NOXAM_COMMAND_DOWNLOADEXECUTE 8
+#define NOXAM_COMMAND_REMOTEPROCESSNORESULT 9
+#define NOXAM_COMMAND_TCPFLOOD 10
+#define NOXAM_COMMAND_UPDATE 11
+#define NOXAM_COMMAND_SCREENSHOT 12
+#define NOXAM_COMMAND_UDPFLOOD 13
+#define NOXAM_COMMAND_FORMGRAB 14
+#define NOXAM_COMMAND_STEALERS 15
+
+typedef void(*NoxamCommandParserDelegate) (int, int, char*);
+typedef void(*NoxamPostPollDelegate) ();
+
+char* NoxamCreateCommand(int NxmCommandID, int NxmCommandType, char *NxmData, int NxmDataLength);
+int NoxamParseCommand(char* NxmCommand, char** NxmCommandData, int* NxmCommandType);
+
+void NoxamFreeCommand(char *NxmData);
+void NoxamFreeParseCommandResult(char *NxmData);
+
+
 #endif NOXAM_NOXAMCOMMAND_HXX
