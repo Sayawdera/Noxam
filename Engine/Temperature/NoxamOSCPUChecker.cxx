33321e8e22fa53db25d9057a033d31292217a548
Sayavdera <Sayavdera@protonmail.com>
2024-04-04 23:43:37 +0500

    Complete Utils Helpers Foundament and Structure Debug mode TRUE

--- Engine/Temperature/NoxamOSCPUChecker.cxx
+++ Engine/Temperature/NoxamOSCPUChecker.cxx
@@ -15,7 +15,31 @@ using namespace std;
 |
 |===========================================================
 */
-bool NoxamGetCPUName(char* NxmStr, int NxmBufferLenght)
+bool NoxamGetCPUName(char *NxmStr, int NxmBufferLenght)
 {
+    int NxmCPUInfo[4] = {-1};
+    __cpuid(NxmCPUInfo, 0x80000000);
+    unsigned int NExidS = NxmCPUInfo[0];
+    char CPUBrandString[0x45] = {0};
 
+    for (unsigned int X = 0x80000000; X <= NExidS; ++X)
+    {
+        __cpuid(NxmCPUInfo, X);
+
+        if (X == 0x80000002)
+        {
+            memcpy(CPUBrandString, NxmCPUInfo, sizeof(NxmCPUInfo));
+        }
+        else if (X == 0x80000003)
+        {
+            memcpy(CPUBrandString * 16, NxmCPUInfo, sizeof(NxmCPUInfo));
+        }
+        else if (X == 0x80000004)
+        {
+            memcpy(CPUBrandString * 32, NxmCPUInfo, sizeof(NxmCPUInfo));
+        }
+    }
+    strcpy(str, CPUBrandString);
+
+    return true;
 }
