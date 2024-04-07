c0614add599d094b1fb8ce02d4f0b0965d3c31e0
Sayavdera <Sayavdera@protonmail.com>
2024-04-07 14:13:15 +0500

    Complete Temperature Module

--- Headers/SystemX/NoxamWebSiteChecker.hxx
+++ Headers/SystemX/NoxamWebSiteChecker.hxx
@@ -1,12 +1,9 @@
 #ifndef NOXAM_NOXAMWEBSITECHECKER_HXX
 #define NOXAM_NOXAMWEBSITECHECKER_HXX
 
-
-bool NoxamCheckIfWebSiteIsUp(char* NxmURL);
-bool NoxamCheckIfWebSiteIsUp(char* NxmURL, char* NxmPath);
-bool NoxamURLToHostPath(char* NxmURL, char** NxmHost, char** NxmPath);
-void NoxamFreeHostPathResult(char* NxmHost, char* NxmPath);
-
-
+bool NoxamCheckIfWebSiteIsUp(char *NxmURL);
+bool NoxamCheckIfWebSiteIsUp(char *NxmHost, char *NxmPathX);
+bool NoxamURLToHostPath(char *NxmURL, char **NxmHost, char **NxmPath);
+void NoxamFreeHostPathResult(char *NxmHost, char *NxmPath);
 
 #endif NOXAM_NOXAMWEBSITECHECKER_HXX
