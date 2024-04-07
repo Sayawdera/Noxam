8a163f2acc4508581cbe48518a87fe03ef9fbbfc
Sayavdera <Sayavdera@protonmail.com>
2024-04-07 10:18:59 +0500

    Updating

--- Engine/Temperature/NoxamOSHWidChecker.cxx
+++ Engine/Temperature/NoxamOSHWidChecker.cxx
@@ -15,7 +15,11 @@ using namespace std;
 |
 |===========================================================
 */
-bool NoxamGetHWid(char* NxmStr, int NxmBufferLength)
+bool NoxamGetHWid(char *NxmStr, int NxmBufferLength)
 {
-
+    HW_PROFILE_INFOA HpI;
+    ZeroMemory(&HpI, sizeof(HW_PROFILE_INFOA));
+    GetCurrentHwProfileA(&HpI);
+    strcpy(NxmStr, HpI.szHwProfileGuid);
+    return TRUE;
 }
